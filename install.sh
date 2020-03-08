97be4031a412b1fcaf454518807378a11bcea9aa
root <mihai.chiroiu@cs.pub.ro>
2020-03-08 14:29:56 +0000

    added containernet to install file

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 # (c) Mihai Chiroiu - CDCI
 
-apt-get update && apt-get upgrade
+# upgrade system
+#sudo apt-get update
+#sudo apt-get -f -y upgrade
+#sudo apt-get -f -y autoremove
+#sudo do-release-upgrade
+
+
+# https://containernet.github.io/#installation
+
+sudo apt-get install -f -y ansible git aptitude
+git clone https://github.com/containernet/containernet.git
+cd containernet/ansible
+sudo ansible-playbook -i "localhost," -c local install.yml
+cd ..
+sudo make develop
