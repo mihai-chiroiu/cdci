b4cd90f4c0a56fe03e3eca6fddfb3dbc2ae5d7f3
root <student@lab.oslo>
2020-03-14 16:00:05 +0000

    enable install

--- install.sh
+++ install.sh
@@ -2,22 +2,21 @@
 # (c) Mihai Chiroiu - CDCI
 
 # upgrade system
-#sudo apt-get update
-#sudo apt-get -f -y upgrade
-#sudo apt-get -f -y autoremove
+sudo apt-get update
+sudo apt-get -f -y upgrade
+sudo apt-get -f -y autoremove
 #sudo do-release-upgrade
 
 
 # https://containernet.github.io/#installation
-#cd ..
-#sudo apt-get install -f -y ansible git aptitude
-#git clone https://github.com/containernet/containernet.git
-#cd containernet/ansible
-#sudo ansible-playbook -i "localhost," -c local install.yml
-#cd ..
-#sudo make develop
+cd ..
+sudo apt-get install -f -y ansible git aptitude
+git clone https://github.com/containernet/containernet.git
+cd containernet/ansible
+sudo ansible-playbook -i "localhost," -c local install.yml
+cd ..
+sudo make develop
 
 # make containers for labs
-#cd cdci
-cd containers
+cd cdci/containers
 make
