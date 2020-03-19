bf08168f87b522371c1ca30580ca780f945a0620
Ruxandra Trandafir <mihai@cs.pub.ro>
2020-03-19 22:21:47 +0200

    added small tweaks for Ubuntu 18.04

--- install.sh
+++ install.sh
@@ -7,6 +7,13 @@ sudo apt-get -f -y upgrade
 sudo apt-get -f -y autoremove
 #sudo do-release-upgrade
 
+# some Ubuntu 18.04 tweaks
+systemctl disable ufw
+sudo systemctl disable systemd-resolved
+sudo systemctl stop systemd-resolved
+sudo echo "nameserver 8.8.8.8"> /etc/resolv.conf
+sudo echo "127.0.0.1 cdci" >> /etc/hosts
+
 
 # https://containernet.github.io/#installation
 cd ..
@@ -20,3 +27,5 @@ sudo make develop
 # make containers for labs
 cd cdci/containers
 make
+
+
