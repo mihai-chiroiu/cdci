021b412aba24afa715065758365911ae4f6f44c2
Mihai Chiroiu <mihai.chiroiu@gmail.com>
2025-11-02 19:26:13 +0000

    small changes to Dockerfile on cdci_base

--- containers/cdci_base/start_scripts/lab03_dhcpd/start_lab03_dhcpd.sh
+++ containers/cdci_base/start_scripts/lab03_dhcpd/start_lab03_dhcpd.sh
@@ -8,7 +8,7 @@ ip route add 10.155.20.0/24 via 10.255.255.4
 ip route add 10.5.140.0/24 via 10.255.255.4
 
 #copy dnsmasq configuration and start the dhcp server
-cp /opt/start-scripts/lab03_dhcpd/cdci_lab03_dhcpd_dnsmaqs.conf /etc/dnsmasq.conf 
+cp /opt/start_scripts/lab03_dhcpd/cdci_lab03_dhcpd_dnsmaqs.conf /etc/dnsmasq.conf 
 service dnsmasq start
 
 # bash infinite loop to prevent container from exiting
